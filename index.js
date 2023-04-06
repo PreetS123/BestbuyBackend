343377fca77efc96264042cf5dcd0436d29deab2
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-06 23:22:28 +0530

    backend to be deployed

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config();
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const connection=require('./config');
+const tvRouter= require('./Routes/Tv.Routes');
+const phoneRouter= require('./Routes/Phone.Routes');
+const laptopRouter= require('./Routes/Laptop.Routes');
+
+app.get('/',(req,res)=>{
+    res.send('Welcome in Backend of Bestbuy_Clone!!')
+})
+app.use('/tv',tvRouter);
+app.use('/phone',phoneRouter);
+app.use('/laptop',laptopRouter);
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT,async()=>{
+    try{
+        await connection
+        console.log('db connected')
+    }
+    catch(err){
+       console.log('check config',err)
+    }
+    console.log(`listening on port ${PORT}`)
+})
